58677f626dfd6485319cc583d86f49fd89ccd0f5
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-11 18:11:12 +0300

    Set general page structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 </script>
 
-<main>
-	<h1 class="text-green-600">Hello App</h1>
-</main>
+<div class="min-h-screen min-w-screen flex flex-col">
+	<nav class="p-8 flex flex-row justify-between w-full">
+		<h1>Title</h1>
+		<div>Icons</div>
+		<p>Something</p>
+	</nav>
+	<main class="p-8 grow flex items-center justify-center">
+		<div class="flex items-center gap-4 border-2 border-black">
+			<div>Form</div>
+			<div class="flex flex-col">
+				<button>Delete</button>
+				<button>Copy</button>
+				<button>More</button>
+			</div>
+			<div>Preview</div>
+		</div>
+	</main>
+	<footer class="p-8">
+		<div>Headers</div>
+		<div>Body (Switched)</div>
+	</footer>
+</div>
