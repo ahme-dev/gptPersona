4caa9225e496ac124413caec3a5195537a3043d8
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-11 20:17:47 +0300

    Add styles for main icons

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Icon from "@iconify/svelte";
+	import Form from "./lib/Form.svelte";
 </script>
 
 <div class="min-h-screen min-w-screen flex flex-col">
@@ -9,20 +10,25 @@
 		<p>Something</p>
 	</nav>
 	<main class="p-8 grow flex items-center justify-center">
-		<div class="flex-center gap-4 border-2 border-black p-4">
+		<div
+			class="flex-center gap-4 border border-black shadow-sm shadow-red-600 p-4 rounded-lg"
+		>
+			<Form />
 			<div class="flex-center flex-col gap-2">
-				<input class="border-2 border-black" type="text" />
-				<input class="border-2 border-black" type="text" />
-			</div>
-			<div class="flex-center flex-col gap-2">
-				<button class="flex-center">
-					<Icon icon="mdi-light:delete" class="h-10 w-10" />
+				<button
+					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-500 active:text-white transition-all"
+				>
+					<Icon icon="mdi-light:delete" class="h-8 w-8" />
 				</button>
-				<button class="flex-center">
-					<Icon icon="mdi-light:clipboard-text" class="h-10 w-10" />
+				<button
+					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-500 active:text-white transition-all"
+				>
+					<Icon icon="mdi-light:clipboard-text" class="h-8 w-8" />
 				</button>
-				<button class="flex-center">
-					<Icon icon="mdi-light:dots-horizontal" class="h-10 w-10" />
+				<button
+					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-500 active:text-white transition-all"
+				>
+					<Icon icon="mdi-light:dots-horizontal" class="h-8 w-8" />
 				</button>
 			</div>
 			<div>Preview</div>
---
+++ src/lib/Form.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="flex-center flex-col gap-2">
+	<input class="border-2 border-black" type="text" />
+	<input class="border-2 border-black" type="text" />
+</div>
