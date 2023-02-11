c101f0988f1ba2efc9dc0a362cef62b452277096
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-11 19:10:19 +0300

    Add iconify icons and some styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Icon from "@iconify/svelte";
 </script>
 
 <div class="min-h-screen min-w-screen flex flex-col">
@@ -8,12 +9,21 @@
 		<p>Something</p>
 	</nav>
 	<main class="p-8 grow flex items-center justify-center">
-		<div class="flex items-center gap-4 border-2 border-black">
-			<div>Form</div>
-			<div class="flex flex-col">
-				<button>Delete</button>
-				<button>Copy</button>
-				<button>More</button>
+		<div class="flex-center gap-4 border-2 border-black p-4">
+			<div class="flex-center flex-col gap-2">
+				<input class="border-2 border-black" type="text" />
+				<input class="border-2 border-black" type="text" />
+			</div>
+			<div class="flex-center flex-col gap-2">
+				<button class="flex-center">
+					<Icon icon="mdi-light:delete" class="h-10 w-10" />
+				</button>
+				<button class="flex-center">
+					<Icon icon="mdi-light:clipboard-text" class="h-10 w-10" />
+				</button>
+				<button class="flex-center">
+					<Icon icon="mdi-light:dots-horizontal" class="h-10 w-10" />
+				</button>
 			</div>
 			<div>Preview</div>
 		</div>
