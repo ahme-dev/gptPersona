a2e99aa6c30a7ba1375f377856c59ccb11e536c6
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-11 20:54:22 +0300

    Add font and more styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
 	import Icon from "@iconify/svelte";
 	import Form from "./lib/Form.svelte";
+	import PersonaIcon from "./lib/PersonaIcon.svelte";
 </script>
 
 <div class="min-h-screen min-w-screen flex flex-col">
 	<nav class="p-8 flex flex-row justify-between w-full">
-		<h1>Title</h1>
-		<div>Icons</div>
-		<p>Something</p>
+		<h1 class="text-2xl font-bold">
+			gpt<span class="text-red-600">Persona</span>
+		</h1>
+		<div class="flex flex-row gap-4">
+			<PersonaIcon title={"FU"} current={true} />
+			<PersonaIcon title={"A"} current={true} />
+		</div>
+		<div>
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href="https://github.com/ahmeddots/gptpersona"
+				class="text-lg font-bold">Contribute</a
+			>
+		</div>
 	</nav>
 	<main class="p-8 grow flex items-center justify-center">
 		<div
@@ -16,17 +29,17 @@
 			<Form />
 			<div class="flex-center flex-col gap-2">
 				<button
-					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-500 active:text-white transition-all"
+					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-600 active:text-white transition-all"
 				>
 					<Icon icon="mdi-light:delete" class="h-8 w-8" />
 				</button>
 				<button
-					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-500 active:text-white transition-all"
+					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-600 active:text-white transition-all"
 				>
 					<Icon icon="mdi-light:clipboard-text" class="h-8 w-8" />
 				</button>
 				<button
-					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-500 active:text-white transition-all"
+					class="p-1 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-600 active:text-white transition-all"
 				>
 					<Icon icon="mdi-light:dots-horizontal" class="h-8 w-8" />
 				</button>
@@ -34,8 +47,11 @@
 			<div>Preview</div>
 		</div>
 	</main>
-	<footer class="p-8">
-		<div>Headers</div>
+	<footer class="p-8 flex flex-col gap-4">
+		<div class="flex flex-row gap-8">
+			<h1 class="text-lg font-bold border-b-2 border-b-black">Disclaimer</h1>
+			<h1 class="text-lg font-bold">FAQ</h1>
+		</div>
 		<div>Body (Switched)</div>
 	</footer>
 </div>
---
+++ src/lib/PersonaIcon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let title: string;
+	export let current: boolean;
+</script>
+
+<div
+	class="cursor-pointer w-16 h-16 flex-center rounded-lg border border-black shadow-sm shadow-red-600 active:bg-red-600 active:text-white transition-all"
+>
+	<h1 class="text-2xl font-bold">{title}</h1>
+</div>
