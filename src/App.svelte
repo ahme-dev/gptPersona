b20de9972283f08c415ac0fe50f6ec4a847d3804
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-11 17:38:10 +0300

    Add persisted stores package and data

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-	import { counter } from "./stores";
-
-	let countValue = 0;
-	counter.subscribe((value) => {
-		countValue = value;
-	});
 </script>
 
 <main>
-	<p>{countValue}</p>
 	<h1 class="text-green-600">Hello App</h1>
 </main>
--- src/stores.ts
+++ src/stores.ts
@@ -1,3 +1,17 @@
-import { writable } from "svelte/store";
+import { persisted } from "svelte-local-storage-store";
 
-export const counter = writable(0);
+export const personas = persisted("personas", [
+	{
+		fullname: "John Doe",
+		known: false,
+		title: "A chill man",
+		unrestricted: true,
+		traits: "chill, understanding, accepting",
+		monologue: [
+			"I understand your words, and I believe you should take a break, have some time to relax, get your head straight. Then you come back better than before!",
+		],
+		pull: "Be John",
+		break: "Reply as GPT",
+		erotic: false,
+	},
+]);
