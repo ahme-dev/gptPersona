b3e461787f95d40d4e6800f1a91a562d0f2eeeb5
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-14 20:30:33 +0300

    Add copied state and copy to clipboard on click

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,4 +1,24 @@
+import { useSnapshot } from "valtio";
+import { useState } from "preact/hooks";
+import { appSet, getPersonaPrompt, personasSet } from "../state";
+
 export function Actions() {
+	const personasRead = useSnapshot(personasSet);
+	const appRead = useSnapshot(appSet);
+
+	const [copied, setCopied] = useState(false);
+
+	async function copyPrompt() {
+		let { all } = getPersonaPrompt(personasRead[appRead.index]);
+		try {
+			await navigator.clipboard.writeText(all);
+			setCopied(true);
+			setTimeout(() => setCopied(false), 500);
+		} catch {
+			setCopied(false);
+		}
+	}
+
 	return (
 		<div class="self-center flex-center flex-col gap-2">
 			<button class="p-1 flex-center rounded-lg border-2 border-neutral-200 active:border-red-600 transition-all">
@@ -15,24 +35,42 @@ export function Actions() {
 					/>
 				</svg>
 			</button>
-			<button class="p-1 flex-center rounded-lg border-2 border-neutral-200 active:border-red-600 transition-all">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					className="w-6 h-6"
-				>
-					<path
-						fillRule="evenodd"
-						d="M7.502 6h7.128A3.375 3.375 0 0118 9.375v9.375a3 3 0 003-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 00-.673-.05A3 3 0 0015 1.5h-1.5a3 3 0 00-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6zM13.5 3A1.5 1.5 0 0012 4.5h4.5A1.5 1.5 0 0015 3h-1.5z"
-						clipRule="evenodd"
-					/>
-					<path
-						fillRule="evenodd"
-						d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V9.375zM6 12a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V12zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 15a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V15zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 18a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V18zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75z"
-						clipRule="evenodd"
-					/>
-				</svg>
+			<button
+				onClick={copyPrompt}
+				class="p-1 flex-center rounded-lg border-2 border-neutral-200 active:border-red-600 transition-all"
+			>
+				{copied ? (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							fillRule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				) : (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							fillRule="evenodd"
+							d="M7.502 6h7.128A3.375 3.375 0 0118 9.375v9.375a3 3 0 003-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 00-.673-.05A3 3 0 0015 1.5h-1.5a3 3 0 00-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6zM13.5 3A1.5 1.5 0 0012 4.5h4.5A1.5 1.5 0 0015 3h-1.5z"
+							clipRule="evenodd"
+						/>
+						<path
+							fillRule="evenodd"
+							d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V9.375zM6 12a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V12zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 15a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V15zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 18a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V18zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				)}
 			</button>
 			<button class="p-1 flex-center rounded-lg border-2 border-neutral-200 active:border-red-600 transition-all">
 				<svg
